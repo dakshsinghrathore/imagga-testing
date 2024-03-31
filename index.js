3f66a43a49edf7c6458c193e5c2c3804c20065fb
dakshsinghrathore <dakshrathore1729@gmail.com>
2024-03-31 11:42:13 +0530

    code: converted imageUrl to an array

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import OpenAI from "openai";
 import got from 'got';
 
-const imaggaApiKey = 'key';
-const imaggaApiSecret = 'key';
-const openai = new OpenAI({ apiKey: 'key' });
+const imaggaApiKey = 'acc_0131accd0181988';
+const imaggaApiSecret = '8abf78124200e286fcd87f4cc85b5177';
+const openai = new OpenAI({ apiKey: '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' });
 
 async function getKeywordsFromImage(imageUrl) {
     try {
@@ -27,7 +27,7 @@ async function generateSongsList(keywords) {
         const completion = await openai.chat.completions.create({
             messages: [
                 { "role": "system", "content": "You are a helpful assistant." },
-                { "role": "user", "content": `Generate a list of hindi songs based on the keywords: ${keywords}` }
+                { "role": "user", "content": `Generate a list of 10 hindi songs in bulleted form along with the context based on the keywords: ${keywords}` }
             ],
             model: "gpt-3.5-turbo",
         });
@@ -40,7 +40,7 @@ async function generateSongsList(keywords) {
 }
 
 
-const imageUrl = 'https://cdn.britannica.com/60/176060-131-624C5D0C/Ganesha-beginnings-Hindu.jpg';
+const imageUrl = ['https://live.staticflickr.com/65535/49406216912_46847e6d63_b.jpg','https://cdn.ca.emap.com/wp-content/uploads/sites/12/2023/10/Howrah-Bridge-Kolkata-Image-by-Chinmaykp25.jpg','https://assets.editorial.aetnd.com/uploads/2010/07/mahatma-gandhi-gettyimages-102701091-2048x2048-1.jpg'];
 
 (async () => {
     try {
